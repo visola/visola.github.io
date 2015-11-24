f054486ecd535d9b380f1b8388234cb2ae59105f
Vinicius Isola <visola@hubspot.com>
2015-11-23 23:49:40 -0500

    Paginate posts page

--- src/main/js/pages.js
+++ src/main/js/pages.js
@@ -7,11 +7,29 @@ var inputDir;
 var outputDir;
 
 function processIndex(posts) {
-  var index = templates.get(inputDir, 'pages/index')({posts:posts});
-  winston.debug('Writing index to: %s',outputDir+'index.html');
-  fs.writeFile(outputDir+'index.html', index, function (err) {
-    if (err) winston.error('Error while writing index.html', err);
-  });
+  var file, i, output, page,
+    pageCount = 0,
+    pageSize = 10;
+
+  do {
+    page = posts.splice(0, pageSize);
+    if (pageCount == 0) {
+      file = 'index.html';
+    } else {
+      file = 'index-'+pageCount+'.html';
+    }
+    output = templates.get(inputDir, 'pages/index')({
+      hasMore: posts.length > 0,
+      nextPage: pageCount + 1,
+      posts:page
+    });
+
+    winston.debug('Writing index to: %s',outputDir+file);
+    fs.writeFile(outputDir+file, output, function (err) {
+      if (err) winston.error('Error while writing index.html', err);
+    });
+    pageCount++;
+  } while (posts.length > 0);
 }
 
 exports.generatePages = function (buildDir, blogDir, posts) {
