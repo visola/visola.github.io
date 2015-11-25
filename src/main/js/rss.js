4b70ef414e3d68e0c7df9fb940c9aef7d3ed83db
Vinicius Isola <viniciusisola@gmail.com>
2015-11-25 17:10:58 -0500

    Only generate RSS feed if when new post exists

--- src/main/js/rss.js
+++ src/main/js/rss.js
@@ -2,12 +2,13 @@ var fs = require('fs');
 var RSS = require('rss');
 var winston = require('winston');
 
-var outpubDir;
+var outpubDir, rssFile;
 var baseDomain = 'https://visola.github.io/';
 var feedPath = baseDomain+'rss.xml';
 
 function generateFeed (posts) {
-  var i,
+  var i, postDate,
+    lastPost = 0,
     feed = new RSS({
       title: "Vinny's blog",
       description: "Most recent blog posts",
@@ -16,24 +17,39 @@ function generateFeed (posts) {
     });
 
   for (i = 0; i < posts.length; i++) {
+    postDate = new Date(posts[i].date);
+    if (postDate.getTime() > lastPost) {
+      lastPost = postDate.getTime();
+    }
+
     feed.item({
       title: posts[i].title,
       description: posts[i].summary,
       guid: posts[i].date,
       url: baseDomain+posts[i].path,
-      date: new Date(posts[i].date),
+      date: postDate,
       author: 'Vinicius Isola'
     });
   }
 
-  winston.debug('Writing RSS to rss.xml...');
-  fs.writeFile(outputDir+"rss.xml", feed.xml(), function (err) {
-    if (err) winston.error('Error while writing rss.xml', err);
+  fs.stat(rssFile, function (err, stats) {
+    if (err) {
+      winston.error("Error while checking rss.xml status.", err);
+    }
+    if (lastPost > new Date(stats.mtime).getTime()) {
+      winston.debug('Writing RSS to rss.xml...');
+      fs.writeFile(rssFile, feed.xml(), function (err) {
+        if (err) winston.error('Error while writing rss.xml', err);
+      });
+    } else {
+      winston.debug('No new posts, will not generate rss.xml');
+    }
   });
 }
 
 exports.generateFeed = function (buildDir, posts) {
   outputDir = buildDir;
+  rssFile = outputDir + 'rss.xml';
   winston.info('Generating RSS...');
   try {
     generateFeed(posts);
